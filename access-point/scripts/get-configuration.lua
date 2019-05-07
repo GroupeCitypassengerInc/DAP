9353f50ad29322ed8270e6b86f8fdd2a6ea0b35a
AlexandreLe <AlexandreLe@01a71061-a360-455f-b967-44c513875c56>
2019-05-07 21:27:18 +0000

    added hardware configs and ssid change with sed in hostapd file

--- access-point/scripts/get-configuration.lua
+++ access-point/scripts/get-configuration.lua
@@ -18,8 +18,15 @@ local reload = require 'reloader'
 -- API Call - GET /ws/DAP/{mac}
  
 local path_addr = '/sys/devices/platform/ag71xx.0/net/eth0/address'
+local ini_file = '/etc/proxy.ini'
 local mac     = io.popen('/bin/cat ' .. path_addr):read('*l')
 local api_key = io.popen('/bin/cat /root/.ssh/apikey'):read('*l')
+local bssid_base = '70:b3:d5:e7:e'
+local m = string.sub(mac,14,17)
+local mask = string.gsub(m,':','')
+local mask = '0x' .. mask
+local get_hostname = '/sbin/uci get system.@system[0].hostname'
+local hostname = io.popen(get_hostname):read('*l')
 local data =
 {
   localdb=
@@ -31,6 +38,8 @@ local data =
     timeout=7200,
     mac_addr=path_addr,
     secret='',
+    hardware='',
+    ssid=''
   },
   portal=
   {
@@ -55,15 +64,15 @@ end
 url = nil
 
 if resp == nil then
-  parser.save('/etc/proxy.ini',data)
+  parser.save(ini_file,data)
   nixio.syslog('info','No secret and URL.')
 else
   url = resp['portalUrl']
   secret = resp['secret']
-  local cmd = '/usr/bin/test -e /etc/proxy.ini'
+  local cmd = '/usr/bin/test -e ' .. ini_file
   local c   = os.execute(cmd)
   if c == 0 then
-    current_data = parser.load('/etc/proxy.ini')
+    current_data = parser.load(ini_file)
     current_url = current_data.portal.url
     current_secret = current_data.ap.secret
     if current_url == url and current_secret == secret then
@@ -71,7 +80,7 @@ else
     else
       data.ap.secret=secret
       data.portal.url=url
-      parser.save('/etc/proxy.ini',data)
+      parser.save(ini_file,data)
       nixio.syslog('info','Configuration saved.')
       reload.uhttpd()
     end
@@ -79,63 +88,51 @@ else
     nixio.syslog('info','No proxy.ini file found. Creating a new one.')
     data.ap.secret=secret
     data.portal.url=url
-    parser.save('/etc/proxy.ini',data)
+    parser.save(ini_file,data)
     nixio.syslog('info','Configuration saved.')
     reload.uhttpd()
   end
 end
 
---- UPDATE HOSTAPD CONF FILE
-local hostapds = resp['files']
-local f = io.open('/etc/hostapd.0.conf')
-local now0 = f:read('*a')
-f:close()
-local f = io.open('/etc/hostapd.1.conf')
-local now1 = f:read('*a')
-f:close()
-local x = hostapds['/etc/hostapd.0.conf'] == now0
-local y = hostapds['/etc/hostapd.1.conf'] == now1
-if x and y then
-  local p = '/usr/bin/test -e /tmp/hostapd.0.pid'
-  local q = '/usr/bin/test -e /tmp/hostapd.1.pid'
-  local exit_code = os.execute(p .. ' && ' .. q)
-  if exit_code ~= 0 then
-    local kill = '/usr/bin/killall hostapd'                                      
-    local k = os.execute(kill)                                                   
-    if k ~= 0 then                                                               
-      nixio.syslog('info','no hostapd killed')                                   
-    end
-    os.execute('sleep 1')
-    reload.hostapd('/etc/hostapd.0.conf')
-    reload.hostapd('/etc/hostapd.1.conf')
-    reload.bridge()
-    reload.dnsmasq()
-    reload.dnsmasq_portal()
-    reload.logger()
+local function format_mask(s)
+  while (string.len(s) < 3) do
+    s = '0' .. s
   end
-  nixio.syslog('info','no changes')
-else
-  local kill = '/usr/bin/killall hostapd'
-  local k = os.execute(kill)
-  if k ~= 0 then
-    nixio.syslog('info','no hostapd killed')
+  return s
+end
+
+--- UPDATE HOSTAPD HEADER (HARDWARE CONFIGURATION)
+local hardware_new = ''
+for k,v in pairs(resp['files']) do
+  hardware_new = hardware_new .. v
+end
+if data.ap.hardware ~= hardware_new then
+  local cmd = '/usr/bin/killall hostapd'
+  local x = os.execute(cmd)
+  if x ~= 0 then
+    nixio.syslog('warning','No hostapd killed.')
   end
-  -- wait for killall to have killed all hostapd processes
   os.execute('sleep 1')
-  for path,conf in pairs(hostapds) do
-    local f = io.open(path,'w')
-    f:write(conf)
+  data.ap.hardware = ''
+  for k,v in pairs(resp['files']) do
+    mask = (mask + i) % 4096
+    local n = mask
+    local s = string.format('%x',n)
+    local s = format_mask(s)
+    local suffix = string.sub(s,1,1) .. ":" .. string.sub(s,2,3)
+    local bssid = base_bssid .. suffix
+    f = io.open(k,'w')
+    f:write(v)
+    f:write('bssid=' .. bssid.. '\nbridge=bridge1\nssid=Borne Autonome')
     f:close()
-    reload.hostapd(path)
+    data.ap.hardware = data.ap.hardware .. v
+    i = i + 1
+    reload.hostapd(k)
   end
   reload.bridge()
   reload.dnsmasq()
-  local cmd = '/usr/bin/test -e /tmp/dnsmasq-portal.pid'
-  local x = os.execute(cmd)
-  if x ~= 0 then
-    reload.dnsmasq_portal()
-    reload.logger()
-  end
+  data.ap.ssid = 'Borne Autonome'
+  parser.save(ini_file,data)
 end
 
 --[[
@@ -176,32 +173,48 @@ end
 ------------------------
 
 
-local cmd = '/usr/bin/curl "%s/index.php?digilan-token-action=configure&digilan-token-secret=%s"'
-local cmd = string.format(cmd,url,secret)
-local resp = io.popen(cmd):read('*a')
+local cmd = '/usr/bin/curl "%s/index.php?' ..
+'digilan-token-action=configure&digilan-token-secret=%s&hostname=%s"'
+local cmd = string.format(cmd,url,secret,hostname)
+local wp_resp = io.popen(cmd):read('*a')
 
-resp = json.parse(resp)
+wp_resp = json.parse(wp_resp)
+
+--- UPDATE SSID
+local ssid_new = wp_resp['ssid']
+if data.ap.ssid ~= ssid_new then
+  change_ssid = "/bin/sed -i 's#^ssid=.*#ssid=%s#g' %s"
+  for k,v in pairs(resp['files']) do
+    local s = string.format(change_ssid,ssid_new,k)
+    local t = os.execute(s)
+    if t ~= 0 then
+      nixio.syslog('err','Failed to change ssid in ' .. k)
+    end
+  end
+  data.ap.ssid = ssid_new
+  parser.save(ini_file,data)
+end
 
 -- Update timeout
-data = parser.load('/etc/proxy.ini')
+data = parser.load(ini_file)
 --- Update timeout
 if tonumber(resp['timeout']) == nil then
   nixio.syslog('err',resp['timeout'] .. ' is not a number.')
   return false
 end
-if data.ap.timeout ~= resp['timeout'] then
+if data.ap.timeout ~= wp_resp['timeout'] then
   data.ap.timeout = resp['timeout']
-  parser.save('/etc/proxy.ini',data)
+  parser.save(ini_file,data)
 else
   nixio.syslog('info','timeout is up to date')
 end
 
 --- Update landing page
 current_landing_page = data.portal.landing_page
-new_landing_page = resp['landing_page']
+new_landing_page = wp_resp['landing_page']
 if current_landing_page ~= new_landing_page then
   data.portal.landing_page = new_landing_page
-  parser.save('/etc/proxy.ini',data)
+  parser.save(ini_file,data)
   nixio.syslog('info','landing page updated')
   reload.uhttpd()
 else
@@ -210,11 +223,39 @@ end
 
 --- INCLUDE PORTAL PAGE
 local portal_page = data.portal.page
-local new_portal_page = resp['portal_page']
+local new_portal_page = wp_resp['portal_page']
 if portal_page ~= new_portal_page then
   data.portal.page = new_portal_page
-  parser.save('/etc/proxy.ini',data)
+  parser.save(ini_file,data)
   reload.uhttpd()
 else
   nixio.syslog('info','portal page is up to date')
+end
+
+--- START SERVICES IF NOT STARTED
+local check = '/usr/bin/test -e /tmp/hostapd.0.pid'
+local s = os.execute(check)
+if s ~= 0 then
+  reload.hostapd('/etc/hostapd.0.conf')
+end
+
+local check = '/usr/bin/test -e /tmp/hostapd.1.pid'
+local s = os.execute(check)
+if s ~= 0 then
+  reload.hostapd('/etc/hostapd.1.conf')
+end
+
+reload.bridge()
+
+local check = '/usr/bin/test -e /tmp/dnsmasq.pid'
+local s = os.execute(check)
+if s ~= 0 then
+  reload.dnsmasq()
+end
+
+local check = '/usr/bin/test -e /tmp/dnsmasq_portal.pid'
+local s = os.execute(check)
+if s ~= 0 then
+  reload.dnsmasq_portal()
+  reload.logger()
 end
