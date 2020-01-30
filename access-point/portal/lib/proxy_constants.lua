38909a953e3758f1c6ade308b9fa1017601fd700
AlexandreLe <AlexandreLe@01a71061-a360-455f-b967-44c513875c56>
2020-01-30 14:36:45 +0000

    no cat

--- access-point/portal/lib/proxy_constants.lua
+++ access-point/portal/lib/proxy_constants.lua
@@ -3,7 +3,7 @@
 -- File loading constants from configuration file
 --
 --]]
-parser    = require "LIP"
+parser    = require 'LIP'
 constants = {}
 
 ini_conf_path  = '/etc/proxy.ini'
@@ -11,8 +11,10 @@ ini_conf_data  = parser.load(ini_conf_path)
 
 function constants.__init__()
   constants.PortalUrl  = ini_conf_data.portal.url
-  local path_mac_addr  = ini_conf_data.ap.mac_addr     
-  constants.ap_mac     = io.popen("/bin/cat " .. path_mac_addr):read("*l")
+  local path_mac_addr  = ini_conf_data.ap.mac_addr
+  local f              = io.open(path_mac_addr,'r')
+  constants.ap_mac     = f:read('*l')
+  f:close()
   constants.ap_timeout = tonumber(ini_conf_data.ap.timeout)
   constants.localdb    = ini_conf_data.localdb.path
   constants.tmpdb      = ini_conf_data.localdb.tmp
