a65cac0424d2a22ee6b83741c3d02f1af51ad8a8
AlexandreLe <AlexandreLe@01a71061-a360-455f-b967-44c513875c56>
2020-02-12 08:43:29 +0000

    different bssid and ssid for support mode and fix in troubleshooting

--- scripts/create-hostapd-conf-support.lua
+++ scripts/create-hostapd-conf-support.lua
@@ -1,14 +1,18 @@
 package.path = package.path .. ';/scripts/lib/?.lua'
+package.path = package.path .. ';/portal/lib/?.lua'
 local helper = require 'bssid-helper'
-local sys    = require 'luci.sys'
+local cst    = require 'proxy_constants'
 
+local mac = cst.ap_mac
 local bssid = helper.get_bssid(0)
 local bssid_conf = 'bssid=%s\n'
 local bssid_conf = string.format(bssid_conf,bssid)
 
-local hostname = sys.hostname()
+local m = string.sub(mac,13,17)
+local suffix = string.gsub(m,':','')
+
 local ssid_conf = 'ssid=Solo AP Support %s\n'
-local ssid_conf = string.format(ssid_conf,hostname)
+local ssid_conf = string.format(ssid_conf,suffix)
 
 local f = io.open('/etc/hostapd.support.conf','w')
 f:write('interface=wlan1\n')
@@ -19,7 +23,7 @@ f:write('logger_stdout=-1\n')
 f:write('logger_stdout_level=-1\n')
 f:write('hw_mode=g\n')
 f:write('channel=0\n')
-f:write('bssid=70:b3:d5:e7:ee:d2\n')
+f:write(bssid_conf)
 f:write('bridge=bridge1\n')
 f:write(ssid_conf)
 f:write('wpa=3\n')
@@ -28,5 +32,3 @@ f:write('wpa_pairwise=CCMP TKIP\n')
 f:write('wpa_passphrase=admincity\n')
 f:write('wpa_key_mgmt=WPA-PSK\n')
 f:close()
-
-
