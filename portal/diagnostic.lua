151b37238f4a0d120d0256912008388930dd3736
AlexandreLe <AlexandreLe@01a71061-a360-455f-b967-44c513875c56>
2020-02-13 14:39:17 +0000

    diagnostic in case no network

--- portal/diagnostic.lua
+++ portal/diagnostic.lua
@@ -1,6 +1,7 @@
 package.path = package.path .. ';/portal/lib/?.lua'
 local troubleshooting = require 'troubleshooting'
 local sys = require 'luci.sys'
+local nixio = require 'nixio'
 
 uhttpd.send("Status: 200 OK\r\n")
 uhttpd.send("Content-Type: text/html\r\n\r\n")
@@ -29,41 +30,115 @@ uhttpd.send("<h2>Diagnostic</h2>\r\n")
 uhttpd.send("<p>\r\n")
 uhttpd.send("<a href='/support'>More information</a>\r\n")
 uhttpd.send("</p>\r\n")
-local plugged = troubleshooting.is_port2_plugged()
-if not plugged then
-  uhttpd.send("<div>\r\n")
-  uhttpd.send("Your access point is not plugged correctly, so it does not have access to internet. Please check your connections.\r\n")
-  uhttpd.send("If nothing happens please contact our support service with the content of /support, the link is down below (More information) at support@citypassenger.com\r\n")
-  uhttpd.send("</div>\r\n")
-else
+function print_diagnostic()
+  local plugged = troubleshooting.is_port2_plugged()
+  if not plugged then
+    uhttpd.send("<div>\r\n")
+    uhttpd.send("Your access point is not plugged correctly, so it does not have access to internet. Please check your connections.\r\n")
+    uhttpd.send("If nothing happens please contact our support service with the content of /support, the link is down below (More information) at support@citypassenger.com\r\n")
+    uhttpd.send("</div>\r\n")
+    return false
+  end
+
   local leased = troubleshooting.has_lease()
   if not leased then
     uhttpd.send("<div>\r\n")
     uhttpd.send("Your access point did not get a DHCP lease. Please check your DHCP server.\r\n")
     uhttpd.send("Send a mail and copy paste the content of /support, the link is down below (More information) to our support serivce: support@citypassenger.com\r\n")
     uhttpd.send("</div>\r\n")
-  else
-    local has_portal = troubleshooting.has_access_to_portal()
-    if not has_portal then
-      uhttpd.send("<div>\r\n")
-      uhttpd.send("Your access point can't reach the captive portal. Check your internet connection.\r\n")
-      uhttpd.send("If your internet connection is OK. Please contact our support our support service with the content of /support, the link is down below (More information) at support@citypassenger.com\r\n")
-      uhttpd.send("</div>\r\n")
-    else
-      if not support.has_network() then
-        uhttpd.send("<div>\r\n")
-        uhttpd.send("Your access point does not have access to internet.\r\n")
-        uhttpd.send("Please check your internet connection.\r\n")
-        uhttpd.send("If your internet connection is OK. Please contact our support service with the content of /support, the link is down below (More information) at support@citypassenger.com\r\n")
-        uhttpd.send("</div>\r\n")
-      else
-        uhttpd.send("<div>\r\n")
-        uhttpd.send("Everything seems fine. Wait a minute for your services to come back online.\r\n")
-        uhttpd.send("If you still have access to this page after several minutes. Please contact our support service with the content of /support, the link is down below (More information) at support@citypassenger.com\r\n")
-        uhttpd.send("</div>\r\n")
-      end
-    end
+    return false
   end
+
+  if not support.has_network() then
+    uhttpd.send("<div>\r\n")
+    uhttpd.send("Your access point does not have access to internet.\r\n")
+    uhttpd.send("Please check your internet connection.\r\n")
+    uhttpd.send("If your internet connection is OK. Please contact our support service with the content of /support, the link is down below (More information) at support@citypassenger.com\r\n")
+    uhttpd.send("</div>\r\n")
+    uhttpd.send("</body>\r\n")
+    uhttpd.send("</html>\r\n")
+    return true
+  end
+
+  local has_portal = troubleshooting.has_access_to_portal()
+  if not has_portal then
+    uhttpd.send("<div>\r\n")
+    uhttpd.send("Your access point can't reach the captive portal. Check your internet connection.\r\n")
+    uhttpd.send("If your internet connection is OK. Please contact our support our support service with the content of /support, the link is down below (More information) at support@citypassenger.com\r\n")
+    uhttpd.send("</div>\r\n")
+    uhttpd.send("</body>\r\n")
+    uhttpd.send("</html>\r\n")
+    return true
+  end
+end
+local diag = print_diagnostic()
+
+if diag then
+  uhttpd.send("<h2>Network diagnostic</h2>\r\n")
+  uhttpd.send("<h3>ping 8.8.8.8</h3>\r\n")
+  uhttpd.send(troubleshooting.ping("8.8.8.8"))
+  uhttpd.send("\r\n")
+  uhttpd.send("<h3>ping gateway</h3>\r\n")
+  local gateway_ip = troubleshooting.get_ip_gateway()
+  if not gateway_ip then
+    uhttpd.send("No gateway ip address")
+  else
+    uhttpd.send(troubleshooting.ping(troubleshooting.get_ip_gateway()))
+  end 
+  uhttpd.send("\r\n")
+  uhttpd.send("<h3>DNS</h3>\r\n")
+  if not nixio.getaddrinfo("www.google.com") then
+    uhttpd.send("Could not resolve www.google.com")
+  else
+    uhttpd.send("Successfully resolved www.google.com")
+  end
+  uhttpd.send("\r\n")
+  uhttpd.send("<h3>Netcat google</h3>\r\n")
+  if support.has_network() then
+    uhttpd.send("OK\r\n")
+  else
+    uhttpd.send("KO\r\n")
+  end
+  uhttpd.send("\r\n")
+  uhttpd.send("<h3>cURL google</h3>\r\n")
+  local res = troubleshooting.curl_google()
+  if res.exit ~= 0 then
+    uhttpd.send("cURL failed with exit code: "..res.exit)
+    uhttpd.send("\r\n")
+    uhttpd.send("cURL stderr:\r\n")
+    uhttpd.send(read_file("/tmp/curl_google_stderr"))
+    uhttpd.send("\r\n")
+  end
+  if res.http_code ~= "200" then
+    uhttpd.send("HTTP response: "..res.http_code)
+  end
+  if res.http_code == "200" and res.exit == 0 then
+    uhttpd.send("OK")
+  end
+  uhttpd.send("\r\n")
+  uhttpd.send("<h3>Netcat cityscope</h3>\r\n")
+  if support.netcat_cityscope() then
+    uhttpd.send("OK\r\n")
+  else
+    uhttpd.send("KO\r\n")
+  end
+  uhttpd.send("\r\n")
+  uhttpd.send("<h3>cURL cityscope</h3>\r\n")
+  local res = troubleshooting.curl_cityscope()
+  if res.exit ~= 0 then
+    uhttpd.send("cURL failed with exit code: "..res.exit)
+    uhttpd.send("\r\n")
+    uhttpd.send("cURL stderr:\r\n")
+    uhttpd.send(read_file("/tmp/curl_cityscope_stderr"))
+    uhttpd.send("\r\n")
+  end
+  if res.http_code ~= "200" then
+    uhttpd.send("HTTP response: "..res.http_code)
+  end
+  if res.http_code == "200" and res.exit == 0 then
+    uhttpd.send("OK")
+  end
+  uhttpd.send("\r\n")
 end
 
 uhttpd.send("</body>\r\n")
